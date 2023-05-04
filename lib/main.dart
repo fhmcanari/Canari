768dfad7027b9aa0f8fbf40ce4b65a9ee39d4311
Hasan M. Hallak <hasanm.hallak@gmail.com>
2023-05-04 14:20:46 +0300

    fix purpos string

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:bloc/bloc.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shopapp/shared/bloc_observer.dart';
@@ -15,53 +15,48 @@ import 'package:connectivity/connectivity.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-Future<bool>checkupdate() async {
+Future<bool> checkupdate() async {
   final info = await PackageInfo.fromPlatform();
   String appVersion = info.buildNumber;
   printFullText(appVersion);
   RemoteConfig remoteConfig = RemoteConfig.instance;
   remoteConfig.setConfigSettings(RemoteConfigSettings(
       fetchTimeout: Duration(seconds: 60),
-      minimumFetchInterval: Duration(seconds: 1)
-  ));
+      minimumFetchInterval: Duration(seconds: 1)));
   await remoteConfig.fetchAndActivate();
   String remoteConfigVersion = remoteConfig.getString('appVersion');
   printFullText(remoteConfigVersion);
-  if (remoteConfigVersion.compareTo(appVersion)==1)
+  if (remoteConfigVersion.compareTo(appVersion) == 1)
     return true;
   else
     return false;
 }
 
-
-
 void main() async {
-
   Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
-  ErrorWidget.builder=(FlutterErrorDetails details)=>Scaffold(
+  ErrorWidget.builder = (FlutterErrorDetails details) => Scaffold(
       backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(Icons.error_outline,size: 60,color: AppColor),
+            Icon(Icons.error_outline, size: 60, color: AppColor),
             height(5),
-            Text('! معذرة ',style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w500
-            ),),
+            Text(
+              '! معذرة ',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
             height(20),
             Text('حدث خطأ ما. أعد المحاولة من فضلك'),
-
           ],
         ),
-      )
-  );
+      ));
   DioHelper.init();
   Cachehelper.init();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.requestPermission();
   runApp(MyApp());
 }
 
@@ -71,20 +66,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   final Connectivity _connectivity = Connectivity();
   ConnectivityResult _connectivityResult = ConnectivityResult.none;
   @override
   void initState() {
-
     _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     super.initState();
   }
-  Future<void>_updateConnectionStatus(ConnectivityResult result)async{
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
     setState(() {
       _connectivityResult = result;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -93,18 +88,13 @@ class _MyAppState extends State<MyApp> {
           fontFamily: 'Almarai',
           appBarTheme: const AppBarTheme(
               systemOverlayStyle: SystemUiOverlayStyle(
-                statusBarIconBrightness: Brightness.dark,
-                statusBarColor: Colors.white,
-              )),
-
+            statusBarIconBrightness: Brightness.dark,
+            statusBarColor: Colors.white,
+          )),
         ),
         debugShowCheckedModeBanner: false,
-        home:InializeWidget()
-    );
+        home: InializeWidget());
   }
-
-  
-
 }
 
 class InializeWidget extends StatefulWidget {
@@ -118,12 +108,11 @@ class _InializeWidgetState extends State<InializeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:FutureBuilder(
-          future:checkupdate(),
-          builder: (BuildContext context,snapshot){
-            return snapshot.data==true?Update():SplashScreen();
-          },
-        )
-    );
+        body: FutureBuilder(
+      future: checkupdate(),
+      builder: (BuildContext context, snapshot) {
+        return snapshot.data == true ? Update() : SplashScreen();
+      },
+    ));
   }
 }
